f75032f62e6b46f5a36fbbe1c4dedb02906c0abf
fifer43 <graham@simpsons.me.uk>
2026-05-08 14:21:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "cf83dd50-1e49-41b9-b68f-aa3cd01fa820"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstategraham260508"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
